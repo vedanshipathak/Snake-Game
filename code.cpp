bccbb35f9d90aeac8046838a11a8a45d64c4226d
Vedanshi Pathak <vedanshipathak226@gmail.com>
2024-07-09 13:37:24 +0530

    logic-part

--- code.cpp
+++ code.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#include<conio.h>
+#include <unistd.h>
 using namespace std;
 bool gameOver;
 const int width=20;
@@ -28,7 +30,7 @@ void draw()
        //for top wall
        for(int i=0;i<width;i++)
        {
-        cout<<"#";
+        cout<<"*";
        }
        cout<<endl;
 
@@ -36,32 +38,95 @@ void draw()
        {
         for(int j=0;j<width;j++)
         {
-          if(j==0)//if first make wall we dont need for inside
-          cout<<"#";
-          
+          if(j==0)//if first make wall we dont need for inside    
+          cout<<"*";
+          if(i==y&&j==x)
+          {
+            cout<<"O";
+          }
+          else if(i==fruitY && j==fruitX)
+          cout<<"F";
+          else
           cout<<" ";
           if(j==width-1)
-          cout<<"#";
+          cout<<"*";
           
 
         }
         cout<<endl;
        }
        //for bottom wall
-       for(int i=0;i<width;i++)
+       for(int i=0;i<width+2;i++)
        {
-        cout<<"#";
+        cout<<"*";
        
        }
         cout<<endl;
+        cout<<"Score:"<<score<<endl;
 }
+//keyboard input:
+//w->up
+//a d->left and right respectively
+// s ->down
 void Input()
 {
-
+   //conio for getting character from console window
+   //kbhit willl handle the user input from the keyboard 
+   //use switch case for different keyboard actions
+   if(_kbhit())
+   {
+      switch (_getch())
+      {
+        case 'a':
+        dir=LEFT;
+        break;
+        case 'd':
+        dir=RIGHT;
+        break;
+        case 'w':
+        dir=UP;
+        break;
+        case 's':
+        dir=DOWN;
+        break;
+        case 'x':
+        gameOver=true;
+        break;
+      }
+   }
 }
 void logic()
-{
-
+{ sleep(0.5);//slow the game if you want
+   switch(dir)
+   {
+      case LEFT:
+      x--;//dec the x cordinate by 1 of snake
+      break;
+      case RIGHT:
+      x++;//increase the x coordinate of snake
+      break;
+      case UP:
+      y--;
+      break;
+      case DOWN:
+      y++;
+      break;
+      default:
+      break;
+   }
+   //condition if it hits the boundary then game over
+   if(x>width || x<0||y>height ||y<0)
+   {
+      gameOver=true;
+   }
+   //increase the score when eat the fruit
+   if(x==fruitX && y==fruitY)
+   {
+      score+=10;//incr score
+      //after score inc again place fruit random anywhere
+      fruitX=rand()%width;//within the width of the game
+      fruitY=rand()%height;
+   }
 }
 int main()
 {
@@ -71,6 +136,6 @@ int main()
     draw();
     Input();
     logic();
-    //Sleep(10);//slow the game if you want
+    
    }
 }
